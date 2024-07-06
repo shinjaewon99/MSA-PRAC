0850a5b7903e3e0f7d7083be0f2e17437ac6e472
shinjaewon99 <tlswodnjs99@naver.com>
2024-07-06 14:57:30 +0900

    test : 곱셈 결과에 대한 테스트

---
+++ src/test/java/com/microservices/jw/ch2/MultiplicationResultAttemptControllerTest.java
@@ -0,0 +1,61 @@
+package com.microservices.jw.ch2;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static com.microservices.jw.ch2.MultiplicationResultAttemptController.ResultResponse;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.given;
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@WebMvcTest(MultiplicationController.class)
+class MultiplicationResultAttemptControllerTest {
+
+    @MockBean
+    private MultiplicationService multiplicationService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private JacksonTester<MultiplicationResultAttempt> jsonResult;
+    private JacksonTester<ResultResponse> jsonResponse;
+
+    @BeforeEach
+    void setUp() {
+        JacksonTester.initFields(this, ObjectMapper::new);
+    }
+
+    @Test
+    void postResultReturnCorrect() throws Exception {
+        genericParameterizedTest(false);
+    }
+
+    void genericParameterizedTest(final boolean correct) throws Exception {
+        // given
+        given(mockMvc.checkAttempt(any(MultiplicationResultAttempt.class)))
+                .willReturn(correct);
+
+        User user = new User("jw");
+        Multiplication multiplication = new Multiplication(50, 70);
+        MultiplicationResultAttempt attempt = new MultiplicationResultAttempt(user, multiplication, 3500);
+
+        // when
+        MockHttpServletResponse response = mockMvc.perform(post("/results")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonResult.write(attempt).getJson()))
+                .andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.getContentAsString()).isEqualTo(jsonResponse.write(new ResultResponse(correct)).getJson());
+    }
+}
